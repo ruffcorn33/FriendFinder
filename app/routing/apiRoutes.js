f3376c820db9aba4178ee49c8d46d758989c7d1b
Craig Christensen <ruffcorn@gmail.com>
2018-02-27 18:14:25 -0600

    Some functionality working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,74 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // LOAD DATA
-//==============================================================
+// link the routes to the data in the friends array
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var friends = require('../data/friends');
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ROUTING
-// =============================================================
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 module.exports = function(app){
-  // API GET request
-  app.get('/api/survey', function(req,res){
-    res.json(friends)
+  // API GET request displays a JSON of the friends array daty when the link is 
+  // visited
+
+  app.get('/api/friends', function(req,res){
+    res.json(friends);
   });
 
   // API POST request
-  // handles survey form data after submit
+  // handles survey form data after submit.
+  // by pushing a JSON object to the
+  // friends array.  form data >> server >> array
+
   app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
+    var user = req.body;
+    // console.log(user);
+    // console.log(friends[0].scores.length);
+    // should be {name, imgLink, diff}
+    // initialize lowest diff to be greater max possible diff to allow for a 
+    // perfect match (diff = 0)
+    var bestMatch = {
+      name: '',
+      imgLink: '',
+      lowestDiff: 50
+    };
+
+    // add the logic to find best match here
+    // save user data to vars
+    // loop through friends array
+    for(f=0;f<friends.length;f++){
+      // initialize diff between friends
+      var diff = 0;
+      console.log(friends[f].scores);
+      console.log(user);
+
+      // loop through the array of scores
+      for(s=0;s<friends[f].scores.length;s++){
+        // find absolute value of diff between user score and friend score
+        // accumulate abs.diffs in total diff      
+        console.log('friends score = '+friends[f].scores[s]);
+        console.log(user);  
+        diff += Math.abs(parseInt(friends[f].scores[s]) - parseInt(user.scores[s]));
+        console.log(diff);
+      } //end friend scores array
+
+      // compare to diff in bestMatch object -- if this diff is lower, 
+      // move to bestMatch
+
+      console.log('diff = '+diff+', lowestDiff = '+bestMatch.lowestDiff);
+      if(diff < bestMatch.lowestDiff){
+        bestMatch.name = friends[i].name;
+        bestMatch.imgLink = friends[i].photo;
+        bestMatch.lowestDiff = diff;
+      }
+    } // end of friends array
+
+    // at end return bestMatch
+    res.json(bestMatch);
+
+    // push user data last to avoid matching self
     friends.push(req.body);
-  });
+
+  });  // end of POST
 
 }
- 
--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// server.js is the main JavaScript file.  It creates and configures
+// the Express server (app)
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// DEPENDENCIES
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const express = require("express");
+const bodyParser = require("body-parser");
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Sets up the Express Server
-// =============================================================
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+// BODY-PARSER MIDDLEWARE
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ROUTER
-// The below points our server to a series of "route" files.
-// ================================================================================
+// The code below points the server to "route" files.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Starts the server to begin listening
-// =============================================================
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
