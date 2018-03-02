3ec19915988f869cfc8755f07cce46e1c90aebf5
Craig Christensen <ruffcorn@gmail.com>
2018-03-02 11:08:24 -0600

    Form done. Logic done.

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const bodyParser = require("body-parser");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Sets up the Express Server to serve static files
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+app.use(express.static('public'))
+
 // BODY-PARSER MIDDLEWARE
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
