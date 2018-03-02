75a9c0bca1c31bc243c5eead83ccfc9eafb4fa99
Craig Christensen <ruffcorn@gmail.com>
2018-03-02 09:57:49 -0600

    logic is working.  most of survey done

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,10 +5,50 @@
 // an array of friend objects
 var friends = [
   {
-    "name":"Nancy",
-    "photo":"https://tygertale.files.wordpress.com/2015/04/nancy1.jpg",
-    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 2]
-  }
+    "name":"Tammy",
+    "photo":"https://images.spoilertv.com/cache/firefly/cast-promotional-photos/river-tam-003_FULL.jpg",
+    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 5]
+  },
+  {
+    "name":"Robert",
+    "photo":"https://images-na.ssl-images-amazon.com/images/I/418meP6q66L._SL500_AC_SS350_.jpg",
+    "scores":[4, 2, 3, 5, 4, 2, 2, 5, 3, 5]
+  },
+  {
+    "name":"Carrie",
+    "photo":"hhttps://vignette4.wikia.nocookie.net/p__/images/a/a8/Princess_Leia_Large_Gun_Close.jpg/revision/latest?cb=20120115141843&path-prefix=protagonist",
+    "scores":[1, 5, 3, 3, 1, 5, 1, 1, 3, 1]
+  },
+  {
+    "name":"Bob",
+    "photo":"https://images-na.ssl-images-amazon.com/images/I/41Ol4gQpzaL._SL500_AC_SS350_.jpg",
+    "scores":[4, 5, 5, 5, 1, 2, 3, 1, 5, 1]
+  },
+  {
+    "name":"Jim",
+    "photo":"http://www.writeups.org/wp-content/uploads/James-Tiberius-Kirk-Star-Trek-William-Shatner.jpg",
+    "scores":[4, 2, 5, 5, 1, 2, 2, 5, 4, 5]
+  },
+  {
+    "name":"Darrell",
+    "photo":"http://cdn1-www.dogtime.com/assets/uploads/2012/02/file_9938_Science-fiction-movie-chara.jpg",
+    "scores":[5, 1, 4, 4, 5, 5, 1, 5, 3, 1]
+  },
+  {
+    "name":"Ellen",
+    "photo":"https://vignette.wikia.nocookie.net/avp/images/4/48/Ellen_Ripley.png/revision/latest/scale-to-width-down/200?cb=20130410133154",
+    "scores":[5, 1, 3, 3, 4, 2, 3, 5, 4, 5]
+  },
+  {
+    "name":"Malcolm",
+    "photo":"https://pbs.twimg.com/profile_images/920472164/ff12_400x400.jpg",
+    "scores":[5, 3, 1, 1, 2, 3, 5, 1, 4, 1]
+  },
+  {
+    "name":"Hal",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/1200px-HAL9000.svg.png",
+    "scores":[3, 4, 2, 2, 3, 1, 2, 5, 4, 1]
+  },
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -22,7 +22,7 @@ module.exports = function(app){
 
   app.post("/api/friends", function(req, res) {
     var user = req.body;
-    // console.log(user);
+    console.log('user name is ' + user.name);
     // console.log(friends[0].scores.length);
     // should be {name, imgLink, diff}
     // initialize lowest diff to be greater max possible diff to allow for a 
@@ -39,15 +39,15 @@ module.exports = function(app){
     for(f=0;f<friends.length;f++){
       // initialize diff between friends
       var diff = 0;
-      console.log(friends[f].scores);
-      console.log(user);
+      console.log(friends[f]);
+      console.log(JSON.stringify(user));
 
       // loop through the array of scores
       for(s=0;s<friends[f].scores.length;s++){
         // find absolute value of diff between user score and friend score
         // accumulate abs.diffs in total diff      
         console.log('friends score = '+friends[f].scores[s]);
-        console.log(user);  
+        console.log('users score = ' + user.scores[s]);  
         diff += Math.abs(parseInt(friends[f].scores[s]) - parseInt(user.scores[s]));
         console.log(diff);
       } //end friend scores array
@@ -57,8 +57,8 @@ module.exports = function(app){
 
       console.log('diff = '+diff+', lowestDiff = '+bestMatch.lowestDiff);
       if(diff < bestMatch.lowestDiff){
-        bestMatch.name = friends[i].name;
-        bestMatch.imgLink = friends[i].photo;
+        bestMatch.name = friends[f].name;
+        bestMatch.imgLink = friends[f].photo;
         bestMatch.lowestDiff = diff;
       }
     } // end of friends array
